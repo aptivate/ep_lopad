38db09ccd544beb3689fc68a48a6b693ff64e164
tomd <tomd+git@aptivate.org>
2014-11-17 12:43:07 +0000

    Initial commit

---
+++ hooks.js
@@ -0,0 +1,37 @@
+var path = require('path');
+var eejs = require('ep_etherpad-lite/node/eejs');
+var toolbar = require("ep_etherpad-lite/node/utils/toolbar");
+var hooks = require('ep_etherpad-lite/static/js/pluginfw/hooks');
+
+
+exports.expressCreateServer = function (hook_name, args, cb) {
+  // serve custom.css
+  console.log("hook_name: ", hook_name);
+  console.log("args: ", args);
+  console.log("cb: ", cb);
+  args.app.get( /static\/custom\/index.css$/, function(req, res)
+  {
+    var filePath = path.normalize(__dirname + "/static/index.css");
+    res.sendfile(filePath);
+  });
+
+  args.app.get('/tos.html', function(req, res)
+  {
+    var filePath = path.normalize(__dirname + "/templates/tos.html");
+    res.sendfile(filePath);
+  });
+
+  args.app.get('/privacy.html', function(req, res)
+  {
+    var filePath = path.normalize(__dirname + "/templates/privacy.html");
+    res.sendfile(filePath);
+  });
+}
+
+exports.eejsBlock_indexWrapper = function(hook_name, args, cb) {
+    var logo = '<div id="title" class="center">&gt;(lopad.org)-</div>',
+        initial = args.content,
+        terms = '<div id="terms" class="center">By using this site, you accept our <a href="/privacy.html">privacy policy</a> and agree to our <a href="/tos.html">terms and conditions</a></div>';
+    args.content = logo + initial + terms;
+    return cb();
+};
