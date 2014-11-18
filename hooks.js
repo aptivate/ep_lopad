766613da0af8b27b7022dcbfc9b4826c0e497e57
tomd <tomd+git@aptivate.org>
2014-11-18 16:54:15 +0000

    Use title defined in settings.json

--- hooks.js
+++ hooks.js
@@ -2,7 +2,7 @@ var path = require('path');
 var eejs = require('ep_etherpad-lite/node/eejs');
 var toolbar = require("ep_etherpad-lite/node/utils/toolbar");
 var hooks = require('ep_etherpad-lite/static/js/pluginfw/hooks');
-
+var settings = require('ep_etherpad-lite/node/utils/Settings');
 
 exports.expressCreateServer = function (hook_name, args, cb) {
   args.app.get('/tos.html', function(req, res)
@@ -19,7 +19,7 @@ exports.expressCreateServer = function (hook_name, args, cb) {
 }
 
 exports.eejsBlock_indexWrapper = function(hook_name, args, cb) {
-    var logo = '<div id="title" class="center">&gt;(lopad.org)-</div>',
+    var logo = '<div id="title" class="center">&gt;('+settings.title+')-</div>',
         initial = args.content,
         terms = '<div class="center about"><strong>lopad.org</strong> is a freely available <a href="http://etherpad.org/">etherpad</a> instance hosted by <a class="aptivate" href="http://www.aptivate.org">Aptivate</a> as part of our work to support IT in international development and promote greater participation globally.</div><div id="terms" class="center">By using this site, you accept our <a href="/privacy.html">privacy policy</a> and agree to our <a href="/tos.html">terms and conditions.</a></div>';
     args.content = logo + initial + terms;
